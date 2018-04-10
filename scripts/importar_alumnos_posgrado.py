457122cd21d26f47cbc8c34555bd0fa44772a1b5
Emanuel Pais <emanuel@econo.unlp.edu.ar>
2018-04-10 09:49:08 -0300

    Agregue el codigo para que actualice los usuarios en caso de que existan pero no posean legajo

--- scripts/importar_alumnos_posgrado.py
+++ scripts/importar_alumnos_posgrado.py
@@ -34,9 +34,14 @@ if __name__ == '__main__':
                     dni = a[4].lower().strip()
                     uid = str(uuid.uuid4())
                     #logging.info('importando {} {} {}'.format(nombre, apellido, dni))
-                    cur.execute('select id from users where dni = %s', (dni,))
+                    cur.execute('select id, legajo from users where dni = %s', (dni,))
                     if cur.rowcount > 0:
-                        logging.info('{},{},{},ya existe. no se lo toca'.format(nombre,apellido,dni))
+                        u = cur.fetchone()
+                        if u[1] is None:
+                            logging.info('{},{},{},{},legajo actualizado'.format(nombre,apellido,dni,legajo))
+                            cur.execute('update users set legajo = %s where id = %s',(legajo, u[0]))
+                        else:
+                            logging.info('{},{},{},ya existe. no se lo toca'.format(nombre,apellido,dni))
                     else:
                         logging.info('{},{},{},{},importado'.format(nombre,apellido,dni,legajo))
                         cur.execute('insert into users (id,name,lastname,dni,legajo) values (%s,%s,%s,%s,%s)', (uid, nombre, apellido, dni, legajo))
