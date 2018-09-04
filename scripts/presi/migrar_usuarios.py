160756ebf117d0d000884b7eaff5f7c460cb460d
Pablo Daniel Rey <pablodanielrey@gmail.com>
2018-09-04 16:38:37 -0300

    agregmos el script en migracion de presi

---
+++ scripts/presi/migrar_usuarios.py
@@ -0,0 +1,47 @@
+
+import psycopg2
+from psycopg2.extras import DictCursor
+import os
+import sys
+
+if __name__ == '__main__':
+    conn = psycopg2.connect("host='{}' user='{}' password='{}' dbname={}".format(
+        os.environ['USERS_DB_HOST'],
+        os.environ['USERS_DB_USER'],
+        os.environ['USERS_DB_PASSWORD'],
+        os.environ['USERS_DB_NAME']
+    ))
+    cur = conn.cursor()
+    try:
+        host = sys.argv[0]
+        db = sys.argv[1]
+        user = sys.argv[2]
+        passw = sys.argv[3]
+
+        conn2 = psycopg2.connect("host='{}' user='{}' password='{}' dbname={}".format(host, user, passw, db))
+        cur2 = conn2.cursor(cursor_factory=DictCursor)
+        try:
+            cur2.execute('select id, dni, name, lastname, telephone, gender, country, city, notes, email, upassword, direccion from users_migracion')
+            for m in cur2:
+                cur.execute('select 1 from usuario where id = %s', (m[0],))
+                if cur.rowcount <= 0:
+                    uid = m[0]
+                    dni = m[1]
+                    nombre = m[2]
+                    apellido = m[3]
+                    telefono = m[4]
+                    genero = m[5]
+                    pais = m[6]
+                    ciudad = m[7]
+                    correo = m[9]
+                    c = m[10]
+                    direccion = m[11]
+                    cur.execute("insert into usuario (id, dni, name, lastname, gender, country, city, address) values (%s,%s,%s,%s,%s,%s,%s,%s)", (uid, dni, nombre, apellido, genero, pais, ciudad, direccion))
+                    cur.execute("insert into telephones (id,number,user_id) values (%s,%s,%s)", (str(uuid.uuid4()), telefono, uid))
+            conn.commit()
+
+        finally:
+            conn2.close()
+
+    finally:
+        conn.close()
