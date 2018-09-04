37447b04fe6687ac185677a1f386776d9347d352
Pablo Daniel Rey <pablodanielrey@gmail.com>
2018-09-04 17:20:56 -0300

    el script quedo funcuionando y miguro correctamente las personas

--- scripts/presi/migrar_usuarios.py
+++ scripts/presi/migrar_usuarios.py
@@ -3,6 +3,7 @@ import psycopg2
 from psycopg2.extras import DictCursor
 import os
 import sys
+import uuid
 
 if __name__ == '__main__':
     conn = psycopg2.connect("host='{}' user='{}' password='{}' dbname={}".format(
@@ -22,9 +23,10 @@ if __name__ == '__main__':
         cur2 = conn2.cursor(cursor_factory=DictCursor)
         try:
             cur2.execute('select id, dni, name, lastname, telephone, gender, country, city, notes, email, upassword, direccion from users_migracion')
-            for m in cur2:
+            for m in cur2.fetchall():
                 cur.execute('select 1 from users where id = %s', (m[0],))
                 if cur.rowcount <= 0:
+                    print('agregando')
                     uid = m[0]
                     dni = m[1]
                     nombre = m[2]
@@ -41,7 +43,11 @@ if __name__ == '__main__':
                             cur.execute("insert into users (id, dni, name, lastname, gender, country, city, address) values (%s,%s,%s,%s,%s,%s,%s,%s)", (uid, dni, nombre, apellido, genero, pais, ciudad, direccion))
                             cur.execute("insert into telephones (id,number,user_id) values (%s,%s,%s)", (str(uuid.uuid4()), telefono, uid))
                             conn.commit()
+                            print('agregado')
+                            break
                         except Exception as e:
+                            print(e)
+                            conn.rollback()
                             dni = '{}_{}'.format(dni,i)
             
 
