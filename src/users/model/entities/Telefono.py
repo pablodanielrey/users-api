67f2e1831076cc4a14f72b705dcd70c064e438ce
Miguel Macagno <miguel.macagno@econo.unlp.edu.ar>
2018-09-07 11:12:03 -0300

    Se corrigio faltante de libreria importada

--- src/users/model/entities/Telefono.py
+++ src/users/model/entities/Telefono.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.orm import relationship
 
 from model_utils import Base
