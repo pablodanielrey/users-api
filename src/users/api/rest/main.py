e7df9740aaf170679528af546045a95a70b53845
Pablo Daniel Rey <pablodanielrey@gmail.com>
2018-10-01 14:00:25 -0300

    corregi un error de persmisos en la api. las personas deben poder obtener sus propios correos

--- src/users/api/rest/main.py
+++ src/users/api/rest/main.py
@@ -178,9 +178,19 @@ def actualizar_usuario(uid, token=None):
 @jsonapi
 def correos_de_usuario(uid, cid, token=None):
 
-    prof = warden.has_one_profile(token, ['users-super-admin', 'users-admin'])
-    if not prof['profile']:
-        return ('no tiene los permisos suficientes', 403)
+    admin = False
+    prof = warden.has_all_profiles(token, ['users-super-admin'])
+    if prof and prof['profile']:
+        admin = True
+    else:
+        prof = warden.has_all_profiles(token, ['users-admin'])
+        if prof:
+            admin = prof['profile']
+
+    if not admin:
+        auid = token['sub']
+        if auid != uid:
+            return ('no tiene los permisos suficientes', 403)        
 
     offset = request.args.get('offset',None,int)
     limit = request.args.get('limit',None,int)
