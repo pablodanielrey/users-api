15cc597aaff06a996f84ab4f0e29c83c53bbb59b
Pablo Daniel Rey <pablodanielrey@gmail.com>
2018-10-01 14:14:45 -0300

    agregue mas permisos

--- src/users/api/rest/main.py
+++ src/users/api/rest/main.py
@@ -160,9 +160,19 @@ def crear_usuario(token=None):
 @jsonapi
 def actualizar_usuario(uid, token=None):
 
-    prof = warden.has_one_profile(token, ['users-super-admin', 'users-admin'])
-    if not prof['profile']:
-        return ('no tiene los permisos suficientes', 403)
+    admin = False
+    prof = warden.has_all_profiles(token, ['users-super-admin'])
+    if prof and prof['profile']:
+        admin = True
+    else:
+        prof = warden.has_all_profiles(token, ['users-admin'])
+        if prof:
+            admin = prof['profile']
+
+    if not admin:
+        auid = token['sub']
+        if auid != uid:
+            return ('no tiene los permisos suficientes', 403)   
 
     datos = json.loads(request.data)
     with obtener_session() as session:
